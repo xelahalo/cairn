6dc6172fd0aeedd5ae52d5a458c759c5a7d17b9c
Alex Olah <olahalex8@gmail.com>
2023-08-30 12:53:17 +0200

    Remove docker check, it wasn't working

--- install.sh
+++ install.sh
@@ -8,14 +8,6 @@ error() {
 
 tag=$(date +%s)
 
-# If Docker socket is not mounted
-if [[ ! -S /var/run/docker.sock ]]; then
-	error "Please bind mount in the docker socket to /var/run/docker.sock"
-	error "docker run -v /var/run/docker.sock:/var/run/docker.sock"
-	error "...or make sure you have access to the docker socket at /var/run/docker.sock"
-	exit 1
-fi
-
 if ! [ -x "$(command -v pip3)" ]; then
 	error "pip3 is not installed"
 	exit 1
