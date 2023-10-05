f989ba209a5e20645565e64c2858f36d4426057d
Alex Olah <olahalex8@gmail.com>
2023-10-05 14:36:51 +0200

    Start creating scaffolding in rust

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use clap::Parser;
+use std::process::{Command, Stdio};
+use std::path::Path;
+use std::io::{BufReader, BufRead};
+
+fn main() {
+    // env::set_var("RUST_BACKTRACE", "full");
+    let args = Cli::parse();
+
+    exec_stream("/bin/bash", vec!("cairn.sh", args.mntdir.as_str()))
+}
+
+// Execute command and stream its output to stdout
+pub fn exec_stream<P: AsRef<Path>>(binary: P, args: Vec<&'static str>) {
+    let mut cmd = Command::new(binary.as_ref())
+        .args(&args)
+        .stdout(Stdio::piped())
+        .spawn()
+        .unwrap();
+
+    {
+        let stdout = cmd.stdout.as_mut().unwrap();
+        let stdout_reader = BufReader::new(stdout);
+        let stdout_lines = stdout_reader.lines();
+
+        for line in stdout_lines {
+            println!("Read: {:?}", line);
+        }
+    }
+
+    cmd.wait().unwrap();
+}
+
+#[derive(Parser)]
+struct Cli {
+    mntdir: String,
+}
+
+
